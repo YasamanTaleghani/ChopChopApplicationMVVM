326fcc5a8cfea64240d921e8999689301e4fba12
y.taleghani <y.taleghani@yahoo.com>
2024-02-21 20:55:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/chopchopapplication/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chopchopapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chopchopapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
