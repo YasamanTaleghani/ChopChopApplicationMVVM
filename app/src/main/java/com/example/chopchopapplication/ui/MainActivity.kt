38995fe71ce8e6db0c548ab2806d5f1d1b7b4c7e
y.taleghani <y.taleghani@yahoo.com>
2024-02-26 16:07:52 -0500

    add HomeFragment to MainActivity.kt

--- app/src/main/java/com/example/chopchopapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/chopchopapplication/ui/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.chopchopapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.chopchopapplication.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setCurrentFragment(HomeFragment())
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container, fragment)
+            commit()
+        }
 }
