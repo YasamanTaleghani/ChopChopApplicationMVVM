bbdbaef595db86445f14bde44c2da41ca12ae1ff
y.taleghani <y.taleghani@yahoo.com>
2024-02-26 16:09:38 -0500

    add ShoppingItem.kt class as a model class

---
+++ app/src/main/java/com/example/chopchopapplication/data/local/ShoppingItem.kt
@@ -0,0 +1,13 @@
+package com.example.chopchopapplication.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_table")
+data class ShoppingItem(
+    var name: String,
+    var price: Double,
+    var count: Int
+) {
+    @PrimaryKey(autoGenerate = true) var id: Int? = null
+}
